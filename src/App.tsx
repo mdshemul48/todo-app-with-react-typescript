c833cb61cdf03524dbcfe7ff124cbba34be8e2f7
MD Shimul <mdshemul480@gmail.com>
2021-07-25 19:33:18 +0600

    adding new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Todos from "./components/Todos";
-import todo from "./models/todo";
+import Todo from "./models/todo";
 
 function App() {
-  const todos: todo[] = [{ id: "gg", text: "hello world" }];
+  const todos: Todo[] = [
+    { id: "gg", text: "hello world" },
+    { id: "234234", text: "this is note 2" },
+  ];
   return (
     <div>
       <Todos items={todos} />
