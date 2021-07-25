ebfd3492c3b8ddb596e71bb969207207e1f0bc75
MD Shimul <mdshemul480@gmail.com>
2021-07-25 19:57:53 +0600

    adding newTodo component for adding new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
@@ -8,6 +9,7 @@ function App() {
   ];
   return (
     <div>
+      <NewTodo />
       <Todos items={todos} />
     </div>
   );
