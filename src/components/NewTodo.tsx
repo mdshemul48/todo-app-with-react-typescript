f3110cb0a28be800da59fdf8dc21d880a546bb5c
MD Shimul <mdshemul480@gmail.com>
2021-07-25 23:29:34 +0600

    adding useref for getting todo data

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo: React.FC = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    
+
+    const enteredText = todoTextInputRef.current!.value;
+    if (enteredText.trim().length === 0) {
+      // throw error
+      return;
+    }
   };
+
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input ref={todoTextInputRef} type="text" id="text" />
       <button>Add Todo</button>
     </form>
   );
