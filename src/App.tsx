d0ace563e396984af2b38d72509ac6426c8f3a38
MD Shimul <mdshemul480@gmail.com>
2021-07-28 21:40:00 +0600

    removing todo interface from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
 import { TodoContextProvider } from "./store/todos-context";
 function App() {
   return (
